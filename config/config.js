7e662dce5c93156fe8be713ff30500695d5f7a7c
shahnawaz <shahnawazahmed8290@gmail.com>
2023-10-07 22:26:27 +0530

    added live url by api side

--- config/config.js
+++ config/config.js
@@ -2,3 +2,4 @@ export const user_name = "shahnawaz ahmed"
 export const user_password = "shahnawaz@ahmed"
 
 export const localHostURL = "http://localhost:3000/"
+export const liveHostURL = "https://singapour:com/"
