895704b1a282c10b6db97ffb421dcb8bd698e6bc
shahnawaz <shahnawazahmed8290@gmail.com>
2023-10-07 22:27:45 +0530

    added jwt key by api side

--- config/config.js
+++ config/config.js
@@ -3,3 +3,5 @@ export const user_password = "shahnawaz@ahmed"
 
 export const localHostURL = "http://localhost:3000/"
 export const liveHostURL = "https://singapour:com/"
+
+export const JWT_key = "sdjfw7ryw8v87wsf8gsidvuiwdue"
